bf14a943a71d40fdf806828dd42ad1b1aa45b40d
Grégoire Seux <grego_github@familleseux.net>
2023-06-08 14:03:00 +0200

    ✨ Day 3 part 2
    Change-Id: Ibda8a7db575f0772c0370dd72bbb8808ce17e171

--- day03/day03.go
+++ day03/day03.go
@@ -28,7 +28,8 @@ func main() {
 		if len(line) == 0 {
 			break
 		}
-		triangle, err := parseTriangle(line)
+		fields := strings.Fields(line)
+		triangle, err := parseTriangle(fields)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -37,10 +38,35 @@ func main() {
 		}
 	}
 	log.Print(fmt.Sprintf("Part1: %d", valid_triangle_count))
+
+	// part 2
+	valid_triangle_count = 0
+	// we assume number of lines is a perfect multiple of 3
+	for i := 0; i < len(lines)/3; i++ {
+		fields0 := strings.Fields(lines[3*i+0])
+		fields1 := strings.Fields(lines[3*i+1])
+		fields2 := strings.Fields(lines[3*i+2])
+		// we could transpose but golang does not have a transposition method built-in 🤦
+		triangle0, err := parseTriangle([]string{fields0[0], fields1[0], fields2[0]})
+		triangle1, err := parseTriangle([]string{fields0[1], fields1[1], fields2[1]})
+		triangle2, err := parseTriangle([]string{fields0[2], fields1[2], fields2[2]})
+		if err != nil {
+			log.Fatal(err)
+		}
+		if triangle0.valid() {
+			valid_triangle_count += 1
+		}
+		if triangle1.valid() {
+			valid_triangle_count += 1
+		}
+		if triangle2.valid() {
+			valid_triangle_count += 1
+		}
+	}
+	log.Print(fmt.Sprintf("Part2: %d", valid_triangle_count))
 }
 
-func parseTriangle(line string) (Triangle, error) {
-	fields := strings.Fields(line)
+func parseTriangle(fields []string) (Triangle, error) {
 	if len(fields) != 3 {
 		return Triangle{}, errors.New("Incorrect number of side for this triangle")
 	}
