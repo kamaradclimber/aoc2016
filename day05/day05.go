d4a9955f0e359613b9914d19eb6803a6405257af
Grégoire Seux <grego_github@familleseux.net>
2023-06-09 14:31:51 +0200

    ✨ Day 5 part 2
    Change-Id: I767d44104ac08db9a7c34e871980489682167c4d

--- day05/day05.go
+++ day05/day05.go
@@ -16,22 +16,40 @@ func main() {
 	}
 	input := strings.Trim(string(content), "\n")
 
-	password := make([]string, 0)
+	password_part1 := make([]string, 0)
+	password_part2 := make([]string, 8)
 	index := 0
 
-	for len(password) < 8 {
+	for {
 		candidate := []byte(input + strconv.Itoa(index))
 		hash := md5.Sum(candidate)
 		stringed_hash := hex.EncodeToString(hash[:])
 		truncated, valid := strings.CutPrefix(stringed_hash, "00000")
 		if valid {
-			password = append(password, string(truncated[0]))
+			password_part1 = append(password_part1, string(truncated[0]))
 			log.Printf("Found one character of the password: %s", string(truncated[0]))
+
+			position := truncated[0] - 48
+			if position >= 0 && position < 8 && password_part2[position] == "" {
+				log.Printf("Found one character of the password for part2: %s", string(truncated[1]))
+				password_part2[position] = string(truncated[1])
+
+				must_continue := false
+				for _, c := range password_part2 {
+					if c == "" {
+						must_continue = true
+					}
+				}
+				if !must_continue {
+					break
+				}
+			}
 		}
 		index++
-		if index%10000 == 0 {
+		if index%100000 == 0 {
 			log.Printf("iteration %d", index)
 		}
 	}
-	log.Printf("Part 1: %s", password)
+	log.Printf("Part 1: %s", password_part1[0:8])
+	log.Printf("Part 2: %s", password_part2)
 }
