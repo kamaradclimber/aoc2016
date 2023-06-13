b9b4090bb05d7559c0dec22d7f1031d27b49192b
Grégoire Seux <grego_github@familleseux.net>
2023-06-13 15:44:51 +0200

    ✨ Day 9 part 2
    Change-Id: I321c981c1eaba2106fcd8761899f663a7cd6dbd4

--- day09/day09.go
+++ day09/day09.go
@@ -8,14 +8,14 @@ import (
 	"strings"
 )
 
+var marker_re = regexp.MustCompile(`^\((\d+)x(\d+)\)`)
+
 func main() {
 	content, err := os.ReadFile("input09.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	line := strings.Trim(string(content), "\n")
-
-	marker_re := regexp.MustCompile(`^\((\d+)x(\d+)\)`)
 
 	decompressed_data := make([]string, 0)
 
@@ -40,5 +40,24 @@ func main() {
 		}
 	}
 	full_output := strings.Join(decompressed_data, "")
-	log.Print("Part1: ", full_output, " ", len(full_output))
+	log.Print("Part1: ", len(full_output))
+
+	part2 := length([]byte(line))
+	log.Print("Part2: ", part2)
+}
+
+func length(input []byte) int {
+	if len(input) == 0 {
+		return 0
+	}
+	match := marker_re.FindSubmatch(input)
+	if match != nil {
+		size, _ := strconv.Atoi(string(match[1]))
+		count, _ := strconv.Atoi(string(match[2]))
+		start := len(match[0])
+		stop := start + size
+		return count*length(input[start:stop]) + length(input[stop:])
+	} else {
+		return 1 + length(input[1:])
+	}
 }
