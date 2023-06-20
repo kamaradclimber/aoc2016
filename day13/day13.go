c6bbe5f9d6be3befe60783a0d464252fe108b1f8
Grégoire Seux <grego_github@familleseux.net>
2023-06-20 11:13:49 +0200

    ✨ Day 13 part 2
    Very ugly!
    
    Change-Id: I6d018eb5fc1133f97d53441152c2f4c6c8ac4e0d

--- day13/day13.go
+++ day13/day13.go
@@ -11,23 +11,28 @@ type Coordinates struct {
 	y int
 }
 
-func (c Coordinates) Edges() []dijkstra.Edge {
+func (c Coordinates) OpenSpace() bool {
 	favorite_number := 1364
+	x, y := c.x, c.y
+	sum := x*x + 3*x + 2*x*y + y + y*y + favorite_number
+	bin_value := strconv.FormatInt(int64(sum), 2)
+	count_of_ones := 0
+	for i := 0; i < len(bin_value); i++ {
+		if bin_value[i] == '1' {
+			count_of_ones++
+		}
+	}
+	return count_of_ones%2 == 0
+}
+
+func (c Coordinates) Edges() []dijkstra.Edge {
 	my_edges := make([]dijkstra.Edge, 0)
 	candidates := [][2]int{[2]int{c.x, c.y - 1}, [2]int{c.x - 1, c.y}, [2]int{c.x + 1, c.y}, [2]int{c.x, c.y + 1}}
 	for _, candidate := range candidates {
 		if candidate[0] >= 0 && candidate[1] >= 0 {
-			x, y := candidate[0], candidate[1]
-			sum := x*x + 3*x + 2*x*y + y + y*y + favorite_number
-			bin_value := strconv.FormatInt(int64(sum), 2)
-			count_of_ones := 0
-			for i := 0; i < len(bin_value); i++ {
-				if bin_value[i] == '1' {
-					count_of_ones++
-				}
-			}
-			if count_of_ones%2 == 0 {
-				my_edges = append(my_edges, MyEdge{c, Coordinates{x, y}})
+			dest := Coordinates{candidate[0], candidate[1]}
+			if dest.OpenSpace() {
+				my_edges = append(my_edges, MyEdge{c, dest})
 			}
 		}
 	}
@@ -54,4 +59,26 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Printf("Part1: %d", len(path)-1)
+
+	// for part 2, we are clearly at a disavantage since I choose to use a package that implement dijkstra without exposing the intermediate computation of shortest path.
+	// Let's bruteforce!
+
+	reachable_count := 0
+	for x := 0; x <= 52; x++ {
+		for y := 0; y <= 52; y++ {
+			if (Coordinates{x, y}).OpenSpace() {
+				path, err := dijkstra.ShortestPath(Coordinates{1, 1}, Coordinates{x, y})
+				if err != nil { // ok this is quite ugly!
+					// log.Print(Coordinates{x, y}, err)
+					continue
+				}
+				if len(path)-1 <= 50 {
+					reachable_count += 1
+				}
+			}
+
+		}
+	}
+	log.Printf("Part2: %d", reachable_count)
+
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/flemeur/go-shortestpath v0.0.0-20190814222641-c186ab48e496 // indirect
 	github.com/thoas/go-funk v0.9.3 // indirect
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
 )
